b30fce6ca829999965db225a13c86c0e95fae9d2
Adam Froehlich <adamcfro@gmail.com>
2022-04-29 10:52:54 -0400

    Solution for Code Abbey problem, matchingBrackets

---
+++ matching-brackets.js
@@ -0,0 +1,22 @@
+/**
+ * This function checks whether brackets in a string are in the correct order.
+ * 
+ * @param {String} str - A string parameter
+ * @returns {Boolean} - Returns true if the brackets are in order
+ */
+function matchingBrackets (str) {
+  let left = ['(', '[', '{', '<'];
+  let right = [')', ']', '}', '>'];
+  let brackets = [];
+  for (let i = 0; i < str.length; i++) {
+    if (left.includes(str[i])) {
+      brackets.push(str[i]);
+    } else if (right.includes(str[i])) {
+      let index = right.indexOf(str[i]);
+      if (brackets[brackets.length - 1] === left[index]) {
+        brackets.pop();
+      }
+    }
+  }
+  return brackets.length === 0;
+}
