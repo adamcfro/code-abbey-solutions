4a92071a8ab9330e6481eb14ebef699580365bca
Adam Froehlich <adamcfro@gmail.com>
2022-05-05 09:21:02 -0400

    Solution for Code Abbey problem, Matching Words

---
+++ matching-words.js
@@ -0,0 +1,22 @@
+/**
+ * This function takes an input string and counts how many times each word
+ * in the string is used. Words used more than once are alphabetized and 
+ * returned in sorted order.
+ * 
+ * @param {String} str - A string parameter
+ * @returns {Array} - Returns an array of sorted strings
+ */
+function matchingWords (str) {
+  let repeats = {};
+  let magicalPhrase = [];
+  let splitWords = str.split(' ').sort();
+  for (let elem of splitWords) {
+    repeats[elem] = (repeats[elem] || 0) + 1;
+  }
+  for (let [key, value] of Object.entries(repeats)) {
+    if (value > 1) {
+      magicalPhrase.push(key);
+    }
+  }
+  return magicalPhrase;
+}
